ecc20bddec042bfbad58e1b0a25703e98aef178a
cs068617 <cs068617@cerner.net>
2022-02-23 01:14:34 -0600

    Added Navbar / Mobile Navbar and Button Class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
 import { Link } from "react-router-dom";
-import { MdFingerprint } from "react-icons/md";
+import "./Navbar.css";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+    window.addEventListener("resize", showButton);
+  }, []);
 
   return (
     <>
-      <div className="navbar">
-        <div className="navbar-container container">
-          <Link className="navbar-logo" to="/">
-            <MdFingerprint className="navbar-icon" />
-            Printables
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            {click ? <FaTimes /> : <FaBars />}
+      <IconContext.Provider value={{ className: "react-icons", color: "#000" }}>
+        <div className="navbar-light">
+          <div className="container-light-nav w-container">
+            <div className="navbar-light-brand w-nav-brand">
+              <Link
+                to="/"
+                className="company-name-block"
+                onClick={closeMobileMenu}
+              >
+                PRINTABLES
+              </Link>
+            </div>
+            <div className="menu-icon" onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <span className="navbar-banner" />
+            <nav
+              className={
+                click ? "nav-menu w-nav-menu active" : "nav-menu w-nav-menu"
+              }
+            >
+              <Link
+                to="/start"
+                className="nav-link w-nav-link"
+                onClick={closeMobileMenu}
+              >
+                start
+              </Link>
+              <Link
+                to="/pricing"
+                className="nav-link w-nav-link"
+                onClick={closeMobileMenu}
+              >
+                pricing
+              </Link>
+              <Link
+                to="/about"
+                className="nav-link w-nav-link"
+                onClick={closeMobileMenu}
+              >
+                about
+              </Link>
+              <Link
+                to="/login"
+                className="navbar-light-navbutton w-button"
+                onClick={closeMobileMenu}
+              >
+                sign up / log in
+              </Link>
+            </nav>
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/services" className="nav-links">
-                Services
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/products" className="nav-links">
-                Products
-              </Link>
-            </li>
-          </ul>
         </div>
-      </div>
+      </IconContext.Provider>
     </>
   );
 }
