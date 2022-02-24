905c92826775c1a03dff04440e4dc7a1c0368133
cs068617 <cs068617@cerner.net>
2022-02-24 13:44:51 -0600

    Added most basic pages and updated index.html to remove comments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/HomePage/Home";
+import Start from "./components/pages/StartPage/Start";
+import Pricing from "./components/pages/PricingPage/Pricing";
+import About from "./components/pages/AboutPage/About";
+import Login from "./components/pages/LoginPage/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,7 +12,11 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
+        <Route path="/start" element={<Start />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/components/pages/StartPage/Start.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Start() {
+  return (
+    <>
+      <h1>Start Page</h1>
+    </>
+  );
+}
+
+export default Start;
