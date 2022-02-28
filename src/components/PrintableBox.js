f85409fb9fb9e204aeea28f647895baf5fbb4f9d
casheridan <prcashnation@gmail.com>
2022-02-28 01:35:26 -0600

    Added Firebase config/functionality, add/view printable, and fixed some minor bugs

--- src/components/PrintableBox.js
+++ src/components/PrintableBox.js
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PrintableBox.css';
-import {FaPlusCircle} from 'react-icons/fa'
+import Printable from './Printable';
+import { FaPlusCircle } from 'react-icons/fa';
+import { getDatabase, ref, set, child, get, onValue } from 'firebase/database'
 
 function PrintableBox () {
+    const db = getDatabase();
+    const dbRef = ref(getDatabase());
+
+    let counter = 0;
+    let printRows = [];
+
+    const [rows, setRows] = useState([]);
+    const [data, setData] = useState([]);
+
+    const getPrintables = () => {
+        counter = 0;
+        for(let i = 0; i < data.length; i++) {
+            if (i !== 0) {
+                printRows.push(
+                    <Printable
+                    key={i}
+                    title={data[i].title}
+                    locationUrl={data[i].locationUrl}
+                    projectFiles={data[i].projectFiles}
+                    exportFile={data[i].exportFile}
+                    software={data[i].software}
+                    printer={data[i].printer}
+                    printStatus={data[i].printStatus}
+                    />
+                );
+                    
+                counter++;
+            }
+        }
+        if (rows.length === 0)
+            setRows(...rows, printRows);
+    }
+    
+    get(child(dbRef, 'prints/')).then((snapshot) => {
+        if (data.length !== 0 && printRows.length === 0) {
+            getPrintables();
+        } else {
+            if (snapshot.exists()) {
+                setData(snapshot.val());
+            } else {
+                console.log("No data available");
+            }
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+        
+    const addPrintable = (pTitle, pLocationUrl, pProjectFiles, pExportFile, pSoftware, pPrinter, pPrintStatus) => {
+        counter += 1;
+
+        // Testing data
+        pTitle="yuh"
+        pLocationUrl="thang.com"
+        pProjectFiles="path/path"
+        pExportFile="path/export"
+        pSoftware="Slicer 2"
+        pPrinter="Prusha"
+        pPrintStatus="Working"
+
+        set(ref(db, 'prints/' + `${counter}/`), {
+            title: pTitle,
+            locationUrl: pLocationUrl,
+            projectFiles: pProjectFiles,
+            exportFile: pExportFile,
+            software: pSoftware,
+            printer: pPrinter,
+            printStatus: pPrintStatus
+        });
+
+        window.location.reload();
+    }
+
     return (
         <>
-            <div className='newprint-btn'>
+            <div className='newprint-btn' onClick={() => addPrintable()}>
                 <FaPlusCircle className='new-btn' />
                 <p>New</p>
             </div>
@@ -18,28 +92,7 @@ function PrintableBox () {
                     <div className='print-box'>
                         <div className='print-list'>
                             <ul className='prints'>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
-                                <li className='printable'>Bruh</li>
+                                {rows}
                             </ul>
                         </div>
                     </div>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDS9Ow1Djyzd6OZjw0WUZRiJCGBqFhwC6I",
+  authDomain: "printables-app.firebaseapp.com",
+  databaseURL: "https://printables-app-default-rtdb.firebaseio.com",
+  projectId: "printables-app",
+  storageBucket: "printables-app.appspot.com",
+  messagingSenderId: "110532804043",
+  appId: "1:110532804043:web:78335846c29d4347a6ca4e",
+  measurementId: "G-2V54K8XN39"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
