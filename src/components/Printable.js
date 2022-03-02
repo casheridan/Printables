3cacd027333c7d81752c6f27f23e086e74cdc820
cs068617 <cs068617@cerner.net>
2022-03-02 13:10:35 -0600

    Added refactor to update state instead of refresh page on adding new print. Fixed key and duplication issue

--- src/components/Printable.js
+++ src/components/Printable.js
@@ -1,56 +1,58 @@
 import React, { useState, useEffect } from "react";
-import { FaAngleRight } from 'react-icons/fa';
-import './Printable.css';
+import { FaAngleRight } from "react-icons/fa";
+import "./Printable.css";
 
-function Printable ({
-    title,
-    location,
-    projectFiles,
-    exportFile,
-    software,
-    printer,
-    printStatus
+function Printable({
+  title,
+  location,
+  projectFiles,
+  exportFile,
+  software,
+  printer,
+  printStatus,
 }) {
-    const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
-    const handleClick = () => setClick(!click); 
+  const handleClick = () => setClick(!click);
 
-    return (
-        <>
-            <li className='printable'>
-                <div className="rollout-banner-header" onClick={handleClick}>
-                    <FaAngleRight className={click ? 'right-angle active' : 'right-angle'}/>
-                    <p>{title}</p>
-                </div>
-                <ul className={click ? 'rollout-banner active' : 'rollout-banner'}>
-                    <li>
-                        <p>Print Location URL:</p>
-                        <p>{location}</p>
-                    </li>
-                    <li>
-                        <p>Project File(s):</p>
-                        <p>{projectFiles}</p>
-                    </li>
-                    <li>
-                        <p>Export File:</p>
-                        <p>{exportFile}</p>
-                    </li>
-                    <li>
-                        <p>Slicer/Software Used:</p>
-                        <p>{software}</p>
-                    </li>
-                    <li>
-                        <p>Printer:</p>
-                        <p>{printer}</p>
-                    </li>
-                    <li>
-                        <p>Print Status:</p>
-                        <p>{printStatus}</p>
-                    </li>
-                </ul>
-            </li>
-        </>
-    );
+  return (
+    <>
+      <li className="printable">
+        <div className="rollout-banner-header" onClick={handleClick}>
+          <FaAngleRight
+            className={click ? "right-angle active" : "right-angle"}
+          />
+          <p>{title}</p>
+        </div>
+        <ul className={click ? "rollout-banner active" : "rollout-banner"}>
+          <li>
+            <p>Print Location URL:</p>
+            <p>{location}</p>
+          </li>
+          <li>
+            <p>Project File(s):</p>
+            <p>{projectFiles}</p>
+          </li>
+          <li>
+            <p>Export File:</p>
+            <p>{exportFile}</p>
+          </li>
+          <li>
+            <p>Slicer/Software Used:</p>
+            <p>{software}</p>
+          </li>
+          <li>
+            <p>Printer:</p>
+            <p>{printer}</p>
+          </li>
+          <li>
+            <p>Print Status:</p>
+            <p>{printStatus}</p>
+          </li>
+        </ul>
+      </li>
+    </>
+  );
 }
 
-export default Printable
+export default Printable;
